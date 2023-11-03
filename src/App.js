8925d6ec1a185b92086a7389c02878638e94eb32
RabinarayanS <rabinarayansahoo9348@gamil.com>
2023-11-03 09:55:36 +0530

    Cart and Cart Items

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import HomePage from './customer/Pages/HomePage/HomePage';
 import Footer from './customer/components/Footer/Footer';
 import Product from './customer/components/Product/Product';
 import ProductDetails from './customer/components/ProductDetails/ProductDetails';
+import Cart from './customer/components/Cart/Cart';
+
 
 
 function App() {
@@ -14,7 +16,8 @@ function App() {
       <div>
         {/* <HomePage/> */}
         {/* <Product/> */}
-        <ProductDetails/>
+        {/* <ProductDetails/> */}
+        <Cart/>
       </div>
       <Footer/>
     </div>
