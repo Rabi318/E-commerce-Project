75f5d05dcb53c9d0a1fa5fa7cca1cf2dc663db85
RabinarayanS <rabinarayansahoo9348@gamil.com>
2023-11-03 11:37:04 +0530

    Check out, Address card

---
+++ src/customer/components/AddressCard/AddressCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AddressCard = () =>{
+  return (
+    <div>
+      <div className="space-y-3">
+        <p className="font-semibold">Ram Kappor</p>
+        <p>Mumbai, sitaram nagar, 400341</p>
+        <div className="space-y-1">
+          <p className="font-semibold">phone Number</p>
+          <p>876947930</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AddressCard
