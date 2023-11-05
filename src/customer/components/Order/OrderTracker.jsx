323f2ae00f7009528f2fc8dde1cae4de19a0c14a
RabinarayanS <rabinarayansahoo9348@gamil.com>
2023-11-05 11:29:57 +0530

    OrderDetails and OrderTracker Implement

---
+++ src/customer/components/Order/OrderTracker.jsx
@@ -0,0 +1,26 @@
+import { Label } from "@mui/icons-material";
+import { Step, StepLabel, Stepper } from "@mui/material";
+import React from "react";
+
+
+const steps = [
+  "Placed",
+  "Order Confirmed",
+  "Shipped",
+  "Out For Delivery",
+  "Delivered"
+]
+const OrderTracker = ({activeStep}) => {
+  return(
+    <div className="w-full">
+      <Stepper activeStep={activeStep} alternativeLabel>
+      {steps.map( (Label) => <Step>
+        <StepLabel sx={{color:"#9155fD", fontSize:"44px"}}>{Label}</StepLabel>
+      </Step>)}
+      </Stepper>
+
+    </div>
+  )
+}
+
+export default OrderTracker
